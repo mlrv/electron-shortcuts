d0872b536f93e757ff98a7cb2620012e1536da95
marco <marco.levrero@gmail.com>
2020-12-22 10:52:43 +0100

    Initial implementation of logic

--- src/main.ts
+++ src/main.ts
@@ -1,57 +1,118 @@
-import { KeyCodes, Separator, Modifier, NonModifier } from './types'
+import { Accelerator, Modifier, NonModifier, NormalizedModifier, InputProperty } from "./types"
+import { BrowserWindow, Input } from "electron"
+import { constVoid } from "./utils"
 
-// Split a string given a separator, I.e.
-// SplitOnSeparator<"a,b,c", ","> -> ["a", "b", "c"]
-// SplitOnSeparator<"a,b,c", "."> -> ["abc"]
-type SplitOnSeparator<S extends string, T extends string> =
-  S extends `${infer A}${T}${infer Rest}`
-    ? [A, ...SplitOnSeparator<Rest, T>]
-    : [S]
+// Given a valid accelerator string, split it into
+// its `Modifier`s and `NonModifier`s components
+const split = <S extends string>(
+  accelerator: Accelerator<S>
+): [Modifier[], NonModifier[]] => {
+  const components = accelerator.split("+")
 
-// Split on "+" 
-type SplitOnPlus<S extends string> = SplitOnSeparator<S, Separator>
+  const modifiers = components.filter(
+    (c): c is Modifier => isModifier(c)
+  )
 
-// Given a list of Keycodes, split it into `Modifier`s and `NonModifier`s
-type Separate<S extends string[], T extends { mod: Modifier[], nonMod: NonModifier[] } = { mod: [], nonMod: [] }> =
-  S["length"] extends 0
-    ? T
-    : S extends [infer H, ...infer Rest]
-      ? Rest extends string[]
-        ? H extends Modifier
-          ? T extends { mod: [...infer Mods], nonMod: [...infer NonMods] }
-            ? Mods extends Modifier[]
-              ? NonMods extends NonModifier[]
-                ? Separate<Rest, { mod: [H, ...Mods], nonMod: NonMods }>
-                : never
-              : never
-            : never
-          : H extends NonModifier
-            ? T extends { mod: [...infer Mods], nonMod: [...infer NonMods] }
-              ? Mods extends Modifier[]
-                ? NonMods extends NonModifier[]
-                  ? Separate<Rest, { mod: Mods, nonMod: [H, ...NonMods] }>
-                  : never
-                : never
-              : never
-            : never
-        : never
-      : never
-    
-// Validate whether a combination of `Modifier`s and `NonModifier`s is valid
-// according to the following rules:
-// - At least one `Modifier`s
-// - Exactly one `NonModifier`
-type IsValidCombination<T extends { mod: Modifier[], nonMod: NonModifier[] }> =
-  T["mod"]["length"] extends 0
-    ? false
-    : T["nonMod"]["length"] extends 0
-      ? false
-      : T["nonMod"]["length"] extends 1
-        ? true
-        : false
+  const rest = components.filter(
+    (c): c is NonModifier => !isModifier(c) // something better?
+  )
 
-// Constrain a string to be a valid accelerator
-type Accelerator<S extends string> =
-  IsValidCombination<Separate<SplitOnPlus<S>>> extends true
-    ? S
-    : never
+  return [modifiers, rest]
+}
+
+// `Modifier` type guard
+const isModifier = (
+  str: string
+): str is Modifier => [
+  "Command",
+  "Cmd",
+  "Control",
+  "Ctrl",
+  "CommandOrControl",
+  "CmdOrCtrl",
+  "Alt",
+  "Option",
+  "AltGr",
+  "Shift",
+  "Super"
+].includes(str)
+
+// Normalize a `Modifier` key to a common subset
+const normalizeModifier = (
+  modifier: Modifier
+): NormalizedModifier => {
+  switch (modifier) { // Verify
+    case "Cmd":
+    case "Command":
+    case "CmdOrCtrl":
+    case "CommandOrControl":
+      return "Cmd"
+
+    case "Alt":
+    case "Option":
+      return "Alt"
+
+    case "Control":
+    case "Ctrl":
+      return "Ctrl"
+
+    default:
+      return modifier
+  }
+}
+
+// Given a list of `Modifier`s, return its normalised subset
+const normalizeModifiers = (
+  modifiers: Modifier[]
+): NormalizedModifier[] => [
+  ...new Set(modifiers.map(normalizeModifier)) // see if I want to keep this
+]
+
+// Given a `NormalizedModifier`, map it to the corresponding
+// input property of an Electron Input event
+const normalizedModifierToInputProperty = (
+  normalizedModifier: NormalizedModifier
+): InputProperty => {
+  switch (normalizedModifier) {
+    case "Super": // could do this earlier
+    case "Cmd":
+      return "meta"
+    case "Ctrl":
+    case "AltGr":
+      return "control"
+    case "Shift":
+      return "shift"
+    case "Alt":
+      return "alt"
+  }
+}
+
+// Register a shortcut for the given accelerator string
+export const register = <S extends string>(
+  accelerator: Accelerator<S>,
+  f: () => void,
+  window: BrowserWindow
+): void => {
+  const win = window || window // register on all windows if not present
+
+  const [modifiers, [nonModifier]] = split(accelerator)
+  const inputProperties = normalizeModifiers(modifiers).map(
+    normalizedModifierToInputProperty
+  )
+  
+  const onKeyUp = (): void => constVoid()
+  const onKeyDown = (input: Input): void => {
+    return input.key === nonModifier && inputProperties.every(
+      p => input[p]
+    ) ? f() : constVoid()
+  }
+
+  win.webContents.on('before-input-event', (_, i) =>
+    i.type === 'keyUp'
+      ? onKeyUp()
+      : onKeyDown(i)
+  )
+}
+
+// register('Cmd+Shift+F1', () => {}, null as any)
+// register('Cmd+Ops', () => {})
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,11 @@
-export type KeyCodes =
+type KeyCodes =
   | Modifier
   | LowerAlpha
   | UpperAlpha
+  | Digit
   | FunctionKey
   | Punctuation
-  | Misc // name?
+  | Misc // name??
 
 export type Modifier =
   | "Command"
@@ -29,17 +30,73 @@ type NormalizedModifier =
 
 export type NonModifier = Exclude<KeyCodes, Modifier> 
 
-export type LowerAlpha = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z"
-export type UpperAlpha = `${Uppercase<LowerAlpha>}`
-export type Digit = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "0"
-export type FunctionKey = "F1" | "F2" | "F3" | "F4" | "F5" | "F6" | "F7" | "F8" | "F9" | "F10" | "F11" | "F12" | "F13" | "F14" | "F15" | "F16" | "F17" | "F18" | "F19" | "F20" | "F21" | "F22" | "F23" | "F24"
-export type Punctuation = "!" | "@" | "#" | "$" | "%" | "^" | "&" | "*" | "(" | ":" | "+" | "<" | "_" | ">" | "?" | "~" | "{" | "|" | "}" | "\""| ";" | "=" | "," | "\\" | "-" | "." | "/" | "\`" | "[" |"]" | "\'"
-export type Misc = "Plus" | "Space" | "Tab" | "Backspace" | "Delete" | "Insert" | "Return" | "Enter" | "Up" | "Down" | "Left" | "Right" | "Home" | "End" | "PageUp" | "PageDown" | "Escape" | "Esc" | "VolumeUp" | "VolumeDown" | "VolumeMute" | "MediaNextTrack" | "MediaPreviousTrack" | "MediaStop" | "MediaPlayPause" | "PrintScreen"
+type LowerAlpha = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z"
+type UpperAlpha = `${Uppercase<LowerAlpha>}`
+type Digit = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "0"
+type FunctionKey = "F1" | "F2" | "F3" | "F4" | "F5" | "F6" | "F7" | "F8" | "F9" | "F10" | "F11" | "F12" | "F13" | "F14" | "F15" | "F16" | "F17" | "F18" | "F19" | "F20" | "F21" | "F22" | "F23" | "F24"
+type Punctuation = "!" | "@" | "#" | "$" | "%" | "^" | "&" | "*" | "(" | ":" | "+" | "<" | "_" | ">" | "?" | "~" | "{" | "|" | "}" | "\""| ";" | "=" | "," | "\\" | "-" | "." | "/" | "\`" | "[" |"]" | "\'"
+type Misc = "Plus" | "Space" | "Tab" | "Backspace" | "Delete" | "Insert" | "Return" | "Enter" | "Up" | "Down" | "Left" | "Right" | "Home" | "End" | "PageUp" | "PageDown" | "Escape" | "Esc" | "VolumeUp" | "VolumeDown" | "VolumeMute" | "MediaNextTrack" | "MediaPreviousTrack" | "MediaStop" | "MediaPlayPause" | "PrintScreen"
 
-export type Separator = "+"
+type Separator = "+"
 
-export type InputProperty =
+type InputProperty =
   | "shift"
   | "control"
   | "alt"
   | "meta"
+
+// Split a string given a separator, I.e.
+// SplitOnSeparator<"a,b,c", ","> -> ["a", "b", "c"]
+// SplitOnSeparator<"a,b,c", "."> -> ["abc"]
+type SplitOnSeparator<S extends string, T extends string> =
+  S extends `${infer A}${T}${infer Rest}`
+    ? [A, ...SplitOnSeparator<Rest, T>]
+    : [S]
+
+// Split on "+" 
+type SplitOnPlus<S extends string> = SplitOnSeparator<S, Separator>
+
+// Given a list of Keycodes, split it into `Modifier`s and `NonModifier`s
+type Separate<S extends string[], T extends { mod: Modifier[], nonMod: NonModifier[] } = { mod: [], nonMod: [] }> =
+  S["length"] extends 0
+    ? T
+    : S extends [infer H, ...infer Rest]
+      ? Rest extends string[]
+        ? H extends Modifier
+          ? T extends { mod: [...infer Mods], nonMod: [...infer NonMods] }
+            ? Mods extends Modifier[]
+              ? NonMods extends NonModifier[]
+                ? Separate<Rest, { mod: [H, ...Mods], nonMod: NonMods }>
+                : never
+              : never
+            : never
+          : H extends NonModifier
+            ? T extends { mod: [...infer Mods], nonMod: [...infer NonMods] }
+              ? Mods extends Modifier[]
+                ? NonMods extends NonModifier[]
+                  ? Separate<Rest, { mod: Mods, nonMod: [H, ...NonMods] }>
+                  : never
+                : never
+              : never
+            : never
+        : never
+      : never
+    
+// Validate whether a combination of `Modifier`s and `NonModifier`s is valid
+// according to the following rules:
+// - At least one `Modifier`s
+// - Exactly one `NonModifier`
+type IsValidCombination<T extends { mod: Modifier[], nonMod: NonModifier[] }> =
+  T["mod"]["length"] extends 0
+    ? false
+    : T["nonMod"]["length"] extends 0
+      ? false
+      : T["nonMod"]["length"] extends 1
+        ? true
+        : false
+
+// Constrain a string to be a valid accelerator
+export type Accelerator<S extends string> =
+  IsValidCombination<Separate<SplitOnPlus<S>>> extends true
+    ? S
+    : never
